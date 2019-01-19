d9bc4e94928783fa51389dd4bf1eb7c25ad29b09
Kotaro Mochizuki <mochizuki@basicinc.jp>
2019-01-19 17:00:45 +0900

    feat: add UserResponse

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -25,6 +27,10 @@ type UsersResponse struct {
 	Result []User
 }
 
+type UserResponse struct {
+	Status int
+	Result User
+}
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	ping := Ping{http.StatusOK, "hello repository pattern"}
 	res, _ := json.Marshal(ping)
@@ -52,8 +58,27 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	dump, _ := httputil.DumpRequest(r, true)
+	fmt.Println(string(dump))
+	ID := strings.Replace(r.URL.Path, "/user/", "", 1)
+	fmt.Printf("ID: %v\n", ID)
+	db := db.Db()
+	defer db.Close()
+	var u User
+	selectQuery := `select * from users where id = ?`
+	row := db.QueryRow(selectQuery, ID)
+	row.Scan(&u.ID, &u.Name)
+	fmt.Printf("n: %v\n", u)
+	// TODO: 失敗しているときは 200 以外を返したいです。
+	res, _ := json.Marshal(UserResponse{http.StatusOK, u})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/users", usersHandler)
+	http.HandleFunc("/user/", userHandler)
 	http.ListenAndServe(":8080", nil)
 }
